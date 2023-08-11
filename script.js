4bfb7c7a093715298c7de990018f896ced2b59d1
Zaiyan Azeem <zaiyanazeem@gmail.com>
2023-08-11 04:12:01 -0400

    Filters/Home
    Added 'Filters' for my portfolio projects and fixed 'Home' button on nav bar

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
-
-
+//DAD JOKES WHEN CLICK LOGO///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function fetchJoke() {
   fetch('https://icanhazdadjoke.com/', {
       headers: {
@@ -25,7 +23,7 @@ document.querySelector('.logo').addEventListener('click', function(event) {
 });
 
 
-//CHANGING TEXT
+//CHANGING TEXT///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Array of titles
 var titles = [' Photographer', ' Media Editor', ' Athlete', ' Student', ' Gamer'];
@@ -51,7 +49,7 @@ setTimeout(function() {
 }, 2700); // ms delay before the first title change
 
 
-//POPUP CONTACT WINDOW
+//POPUP CONTACT WINDOW///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -79,8 +77,67 @@ if (event.target == modal) {
 }
 }
 
+//FILTER CARDS///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function filterSelection(category, event) {
+  var cards, i;
+  cards = document.getElementsByClassName("card");
 
-//SCROLL NAVBAR
+  for (i = 0; i < cards.length; i++) {
+      if (category == "all" || cards[i].classList.contains(category)) {
+          addClass(cards[i], "show");
+      } else {
+          removeClass(cards[i], "show");
+      }
+  }
+
+  var btns = document.getElementsByClassName("filter-btn");
+  for (i = 0; i < btns.length; i++) {
+      removeClass(btns[i], "active");
+  }
+
+  // Check if the event is passed
+  if(event) {
+      event.currentTarget.className += " active";
+  } else {
+      // If not, find the 'all' button and activate it
+      for(i = 0; i < btns.length; i++) {
+          if(btns[i].innerHTML.toLowerCase() == 'all') {
+              addClass(btns[i], "active");
+              break;
+          }
+      }
+  }
+}
+
+
+
+
+// Helper functions
+function addClass(element, name) {
+  var arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  if (arr1.indexOf(name) == -1) {
+      element.className += " " + name;
+  }
+}
+
+function removeClass(element, name) {
+  var arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (var i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+          arr1.splice(arr1.indexOf(arr2[i]), 1);
+      }
+  }
+  element.className = arr1.join(" ");
+}
+
+window.onload = function() {
+  filterSelection('all');
+
+//SCROLL NAVBAR///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 window.addEventListener('scroll', function() {
   const navbar = document.querySelector('.header');
@@ -89,4 +146,5 @@ window.addEventListener('scroll', function() {
   } else {
       navbar.classList.remove('solid-nav');
   }
-});
+})
+}
